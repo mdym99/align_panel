70f47adf16b52abe1ae856d03d5b45bb556f8425
Michal Dymacek <216893@vutbr.cz>
2023-07-12 10:41:23 +0200

    new version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="align_panel",
-    version='0.0.2',
+    version='0.0.4',
     license='MIT',
     include_package_data=True,
     zip_safe=False,
