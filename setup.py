567cfa484e518e2a38f2bf9b1cb8352de2a22383
Michal Dymacek <216893@vutbr.cz>
2023-07-25 16:04:05 +0200

    new examples + changes in setup

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="align_panel",
-    version='0.0.6',
+    version='0.0.7',
     license='MIT',
     include_package_data=True,
     zip_safe=False,
